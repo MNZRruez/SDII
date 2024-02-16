4d3029a0da01a8cb5f38c79dd70ccb81ca64934f
mnzrali <mnzr.alee@gmail.com>
2024-02-16 22:55:07 +0530

    Update LoginUI

--- LoginUI/src/main/java/com/example/loginui/HelloController.java
+++ LoginUI/src/main/java/com/example/loginui/HelloController.java
@@ -1,15 +1,46 @@
 package com.example.loginui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField logUsername;
 
     @FXML
-    protected void onHelloButtonClick() {
+    private TextField logPassword;
 
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @FXML
+    private TextField firstname;
+
+    @FXML
+    private TextField lastname;
+
+    @FXML
+    private TextField age;
+
+    @FXML
+    private TextField gender;
+
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField signUsername;
+    @FXML
+    private TextField signPassword;
+
+    @FXML
+    protected void onButtonClick() {
+        logUsername.setText("");
+        logPassword.setText("");
+        firstname.setText("");
+        lastname.setText("");
+        age.setText("");
+        gender.setText("");
+        email.setText("");
+        signPassword.setText("");
+        signUsername.setText("");
+
+
     }
 }
